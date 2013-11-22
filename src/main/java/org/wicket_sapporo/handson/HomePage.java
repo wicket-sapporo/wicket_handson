fb8eabd5a42b102016776caa0c22bfade4c68399
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2013-11-22 16:54:21 +0900

    Added initial project files.

---
+++ src/main/java/org/wicket_sapporo/handson/HomePage.java
@@ -0,0 +1,12 @@
+package org.wicket_sapporo.handson;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage() {
+
+	}
+
+}
---
+++ src/main/java/org/wicket_sapporo/handson/WicketApplication.java
@@ -0,0 +1,34 @@
+package org.wicket_sapporo.handson;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without
+ * deploying, run the Start class.
+ *
+ * @see jp.ac.shinshu_u.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<? extends WebPage> getHomePage() {
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init() {
+		super.init();
+		// サーバ・クライアント間のリクエスト・レスポンスの文字エンコード
+		getRequestCycleSettings().setResponseRequestEncoding("UTF-8");
+		// Wicketに取り込まれるHTMLファイルのエンコード
+		getMarkupSettings().setDefaultMarkupEncoding("UTF-8");
+	}
+
+}
