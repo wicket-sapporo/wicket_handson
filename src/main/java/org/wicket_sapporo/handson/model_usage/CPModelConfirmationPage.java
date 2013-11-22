f459c82304a8d7575fb8d5c689318a4fc5514e57
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2013-11-22 17:18:38 +0900

    Added model-answer files.

---
+++ src/main/java/org/wicket_sapporo/handson/model_usage/CPModelConfirmationPage.java
@@ -0,0 +1,19 @@
+package org.wicket_sapporo.handson.model_usage;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
+import org.wicket_sapporo.handson.beans.UserLunch;
+
+public class CPModelConfirmationPage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public CPModelConfirmationPage(IModel<UserLunch> model) {
+		// setDefaultModel(IModel) メソッドで、Modelをページにセットする
+		setDefaultModel(new CompoundPropertyModel<>(model));
+		add(new Label("name"));
+		add(new Label("lunch"));
+	}
+
+}
