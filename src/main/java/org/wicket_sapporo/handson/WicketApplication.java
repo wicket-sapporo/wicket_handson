e15b5dcdef0627ade4fb72845385fd03e69c3d49
CIST yamakawa <yamakawa@photon.chitose.ac.jp>
2013-12-03 19:42:38 +0900

    Adjust returning session.

--- src/main/java/org/wicket_sapporo/handson/WicketApplication.java
+++ src/main/java/org/wicket_sapporo/handson/WicketApplication.java
@@ -1,10 +1,14 @@
 package org.wicket_sapporo.handson;
 
+import org.apache.wicket.Session;
 import org.apache.wicket.core.request.mapper.MountedMapper;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
 import org.apache.wicket.request.mapper.parameter.UrlPathPageParametersEncoder;
 import org.wicket_sapporo.handson.bookmarkable.ParamReceiptPage;
+import org.wicket_sapporo.handson.session.MySession;
 
 /**
  * Application object for your web application. If you want to run this application without
@@ -33,6 +37,13 @@ public class WicketApplication extends WebApplication {
 		getMarkupSettings().setDefaultMarkupEncoding("UTF-8");
 		// URLのマウント
 		mount(new MountedMapper("/param_receipt", ParamReceiptPage.class, new UrlPathPageParametersEncoder()));
+	}
+
+	@Override
+	public Session newSession(Request request, Response response) {
+		// return super.newSession(request, response);
+		// 独自に拡張したSessionの利用
+		return new MySession(request);
 	}
 
 }
