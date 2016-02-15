52f91113d08dc0ac84037d15e21d347c180838da
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2016-02-15 12:52:44 +0900

    Adjust default test-case

--- src/test/java/org/wicket_sapporo/handson/TestHomePage.java
+++ src/test/java/org/wicket_sapporo/handson/TestHomePage.java
@@ -4,27 +4,20 @@ import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Simple test using the WicketTester
- */
-public class TestHomePage
-{
-	private WicketTester tester;
+public class TestHomePage {
+  private WicketTester tester;
 
-	@Before
-	public void setUp()
-	{
-		tester = new WicketTester(new WicketApplication());
-	}
+  @Before
+  public void setUp() {
+    tester = new WicketTester(new WicketApplication());
+  }
 
-	@Test
-	public void homepageRendersSuccessfully()
-	{
-		//start and render the test page
+  @Test
+  public void ページの表示が成功する() {
+    // start and render the test page
     tester.startPage(HomePage.class);
 
-		//assert rendered page class
+    // assert rendered page class
     tester.assertRenderedPage(HomePage.class);
-
-	}
+  }
 }
